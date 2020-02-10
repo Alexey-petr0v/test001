4c692096d078575729550afad1d6d1998a3cb3c8
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-02-10 15:50:27 +0700

    Instaled babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import $ from 'jquery';
+//import $ from 'jquery';
 import avg from './some.js';
 
-$('.title').html('Some text!');
+//$('.title').html('Some text2!');
 
 console.log(avg(1, 4, 8));
+
+async function app() {
+    await fetch('fmkl');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
 
 let conf = {
-    entry: './src/index.js',
+    entry: ['@babel/polyfill', './src/index.js'],
     output: {
         path: path.resolve(__dirname+ '/dist/'),
-        filename: 'main.js'
+        filename: 'main.js',
+        publicPath: 'dist/' /* для работы npm run build */
+    },
+    devServer: { /* для отображения ошибок в браузере */
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/' /* исключить папку из перегона через babel-loader */
+            }
+        ]
     }
 };
 
